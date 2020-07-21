b590f3367087e12cf25a88fdb0da782020bf19fb
prince <princeakpabio90@gmail.com>
2020-07-21 20:36:23 +0100

    Updates

---
+++ precache-manifest.94a47c9161b5c07875be415fc07b5715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7337f724982618d535db5c821cfc342",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b51399c2c60bc5b75917",
+    "url": "/monsters-rolodex/static/css/main.442ca0c1.chunk.css"
+  },
+  {
+    "revision": "83708b8d3de79eae6b22",
+    "url": "/monsters-rolodex/static/js/2.833f470a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.833f470a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51399c2c60bc5b75917",
+    "url": "/monsters-rolodex/static/js/main.fe22d1b0.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
