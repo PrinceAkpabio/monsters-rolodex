fd150f3c947246aed12c1dab308da9b10dac2cd2
prince <princeakpabio90@gmail.com>
2021-01-02 11:40:06 +0100

    added mobile responsive media queries

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,7 @@ import Card from '../card/card.component'
 import './card-list.styles.css'
 
 const CardList = (props) => {
-//  console.log(props);
+//  console.log(props.monsters.id);
  return <div className='card-list' >
 
   {props.monsters.map(monster => <Card key={monster.id} monster={monster}/>)}
